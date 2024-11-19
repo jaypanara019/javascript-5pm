c5ed4749d7aaf7ea532e8dce4a1a05fd3f917841
jaypanara019 <160013245+jaypanara019@users.noreply.github.com>
2024-11-19 18:49:35 +0530

    19-11-2024 (jay-panara)

---
+++ output.js
@@ -0,0 +1,23 @@
+// output methaod
+
+// 1.console.log()
+// 2.alert()
+// 3.document.write()
+// 4.dom = id
+
+
+
+// 1. CONSLOE.LOG
+// console.log('red & white')
+// console.log(5+8)
+
+
+// 2.ALERT
+// alert('plz support me')
+
+// 3.DOCUMENT.WRITE
+// document.write('hello gues')
+// document.write('my frd name is rahul')
+
+// 4.DOM = ID
+// document.getElementById('jay').innerHTML = "helooooooooo"
